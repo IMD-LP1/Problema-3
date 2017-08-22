b410226b041a5f13348f2838c19d0c76d47c7c8d
Fernando Rodrigues Maciel <fernandomaciel@A202-09L.imd.local>
2017-08-22 17:17:54 -0300

    Problema-3

---
+++ lists/programa0301.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	int* pointer{new int {42}}
+	std::cout << *pointer << std::endl;
+
+	*pointer = 10;
+	std::cout << *pointer << std::endl;
+
+	delete pointer;
+	pointer = nullptr;
+
+	//std::cout << *pointer << std::endl;
+
+	long* good{new long{}};
+	std::cout << *good << std::endl;
+	delete good;
+
+	//delete good
+
+	good - nullptr;
+
+	//delete good;
+
+	long* bad{new long};
+	std::cout << *bad << std::endl;
+	delete bad;
+	bed = nullptr;
+
+	return EXIT_SUCCESS;
+}
---
+++ lists/programa0304.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	int* p1		= new int;
+	int* p2 	= new int(5);
+	int* p3 	= new int{5};
+	int* p4 	= new int[5];
+	int(* p5)[5] =	new inte[2][3];
+	
+	return EXIT_SUCCESS
+}
