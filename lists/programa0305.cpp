6dff471a9709908dc715267eb8910e9d30027adf
Cleydson Talles Araujo Vieira <talles17@A307-35L.imd.local>
2017-08-24 20:11:05 -0300

    questao5 comentada

--- lists/programa0305.cpp
+++ lists/programa0305.cpp
@@ -1,3 +1,5 @@
+/** @file programa0305.cpp	*/
+/** @@question 5		*/
 #include <iostream>
 using std::cout ;
 using std::cin ;
@@ -6,14 +8,22 @@ using std::endl ;
 #include <new>
 
 
-
+/** 
+ *	@brief		Dados do vetor
+ *	@details	tam Valor do tamanho do vetor
+ *	@details	elementos Elementos do vetor		
+*/
 struct Vector{
 	int tam ;
 	double *elementos ;
 } ;
-
+/**
+ *	@brief	Funçao que cria o vetor e o aloca dinamicamente
+ *	@param	v Struct do vetor
+ *	@param	tam Valor do tamanho do vetor
+*/
 void vector_init(Vector& v, int tam) {
-	v.tam =tam ;
+	v.tam = tam ;
 	v.elementos = new(std::nothrow)double[tam] ;
 	if (v.elementos != nullptr) {
 		cout << "Alocaçao feita." << endl ;
@@ -23,19 +33,29 @@ void vector_init(Vector& v, int tam) {
 	}
 
 }
-
+/**
+ *	@brief	Funçao que libera espaço alocado para o vetor
+ *	@param	v Struct do vetor
+*/
 void vector_free (Vector& v) {
 	delete[] v.elementos ;
 	v.elementos = nullptr ;
 }
-
+/**
+ *	@brief	Funçao que recebe os valores do vetor
+ *	@param	v Struct do vetor
+*/
 void vector_read (Vector& v) {
 	for (int ii = 0 ; ii < v.tam ; ii++) {
 		cout << "Elemento[" << ii+1 << "]: " ;
 		cin >> v.elementos[ii] ;
 	}
 }
-
+/**
+ *	@brief	Funçao que calcula a soma de todos os elementos do vetor
+ *	@param	v Struct do vetor
+ *	@return	A soma dos elementos do vetor
+*/
 double vector_sum (Vector& v) {
 	double soma{0} ;
 	for (int ii{0} ; ii < v.tam ; ii++) {
@@ -43,7 +63,12 @@ double vector_sum (Vector& v) {
 	}
 	return soma ;
 }
-
+/**
+ *	@brief	Funçao que busca um valor no vetor
+ *	@param	v Struct do vetor
+ *	@param	key Valor desejado para encontrar no vetor
+ *	@return Resultado da busca 
+*/
 bool vector_find (Vector& v , int key) {
 	for (int ii{0} ; ii < v.tam ; ii++) {
 		if (v.elementos[ii] == key) {
@@ -52,7 +77,9 @@ bool vector_find (Vector& v , int key) {
 	}
 	return false ;
 }
-
+/**
+ *	@brief	Funçao principal
+*/
 int main (int argc , char *argv[]) {
 	int n{0} , chave{0} ;
 	Vector vetor ;
