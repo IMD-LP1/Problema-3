beb5923e6892bdb16118a01a22434b4fe0823a96
Cleydson Talles Araujo Vieira <talles17@A307-34L.imd.local>
2017-08-29 19:50:29 -0300

    questao9 add

--- lists/list1301.cpp
+++ lists/list1301.cpp
@@ -1,19 +1,37 @@
+/** @file list1301.cpp	*/
+/** @@question 9		*/
 #include <cstdio>
 #include <fstream>
 #include <iostream>
 
+
+/**
+ *	@brief	Funçao principal
+*/
 int main () {
-	std::fstream in ("list1301.txt") ;
+	std::fstream in ("list1301.in") ;
 	if (not in) {
-		std::perror("list1301") ;
+		std::perror("list1301.in") ;
 	}
 	else {
-		int x(0) ;
-		while (in >> x) {
-			std::cout << x << '\n' ;
-
+		std::string line ;
+		std::fstream out ("list1301.out") ;
+		if (not out) {
+			std::perror("list1301.out") ;
 		}
-		in.close() ; 
+
+		else {
+
+			while (!in.eof()) {
+				getline(in, line) ;
+				out << line << '\n' ;
+				
+			}
+			
+			
+			out.close() ; 
+		}
+		in.close() ;
 	}
 }
 
